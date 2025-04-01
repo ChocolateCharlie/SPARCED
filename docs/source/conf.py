ea6c1f2e6d4f6d2cef6af2966284ff730ed27e93
chocolatecharlie <aurore.amrit@umu.se>
2025-04-01 11:16:31 +0200

    Sphinx quickstart

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SPARCED'
+copyright = '2025, Birtwistle Lab (Clemson, SC, USA) & Erdem Lab (Umeå, Västerbotten, Sweden)'
+author = 'Birtwistle Lab (Clemson, SC, USA) & Erdem Lab (Umeå, Västerbotten, Sweden)'
+release = '2.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
